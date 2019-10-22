fb20dc85ec7004c614b4ef2d355aa28fac988ac9
Eva <evamaria.horvath@gmx.at>
2019-10-22 10:35:09 +0200

    newest changes

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ exports = module.experts = app;*/
 
 
 
-// storing dependencies in variables
+// storing dependencies in variables not nessecary for us to know what exactly is going on
 var express = require('express');
 var request = require('request');
 var cheerio = require('cheerio');
@@ -108,7 +108,7 @@ var app = express();
 
 
 // INSTAGRAM SCRAPER: access by going to 'localhost:2100/instagram'
-app.get('/instagram', function(req, res){
+app.get('/instagram', function(req, res){ //this one is really important because this is inside node and as soon as we say in terminal launch this code it will appear, so here we say as soon as somebody goes to this localhost it will appear and the user will see the data 
 
   // try any hashtags and see the results, make sure to write INSIDE the quotation marks
   var hashtag = 'Gutmensch';
@@ -130,7 +130,7 @@ app.get('/instagram', function(req, res){
       res.send(instagram_data);
 
       // save the data we've stored in our object on our machine
-      fs.writeFile('./data/instagram_output.js', 'var instagram_output = ' + instagram_data, function(err){
+      fs.writeFile('./datanew/instagram_output_WEDNESDAY.js', 'var instagram_output = ' + instagram_data, function(err){
         console.log('File is written successfully!');
       });
 
